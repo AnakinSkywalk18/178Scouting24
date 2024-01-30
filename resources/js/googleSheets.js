5bd53872151cfc3a58bb5e968762b562a291b56e
AnakinSkywalk18 <103545694+AnakinSkywalk18@users.noreply.github.com>
2024-01-29 23:56:11 -0500

    Update googleSheets.js

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,5 +1,5 @@
 function setUpGoogleSheets() {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwYERDmyhPxZuFGoZjNbGSAxTj_FCO4bt8USHsMQr7464ctQLUldNA30aGTK93bx0VtGg/exec'
+    const scriptURL = ''
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
  
