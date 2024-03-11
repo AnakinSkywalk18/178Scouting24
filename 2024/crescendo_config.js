b901953b3b5fe57eea9bfc917e04c2298cab3842
AnakinSkywalk18 <103545694+AnakinSkywalk18@users.noreply.github.com>
2024-03-11 15:49:58 -0400

    Update crescendo_config.js
    2024ctwat --> 2024mawne

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024ctwat",
+      "defaultValue": "2024mawne",
       "required": "true"
     },
     { "name": "Match Level",
